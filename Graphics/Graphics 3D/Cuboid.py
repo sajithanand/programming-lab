837bfd99060146ed851491ae0f8ef01929efc3cd
sajithanand <94071830+sajithanand@users.noreply.github.com>
2022-02-02 14:40:25 +0530

    Add files via upload

---
+++ Graphics/Graphics 3D/Cuboid.py
@@ -0,0 +1,7 @@
+def cubarea(l,b,h):
+    cubarea=2*[(l*b)+(l*h)+(b*h)]
+    return cubarea
+
+def cubperi(l,b,h):
+    cubperimtr=4*(l+b+h)
+    return cubperimtr
---
+++ Graphics/Graphics 3D/Sphere.py
@@ -0,0 +1,9 @@
+def spherearea(r):
+    import math
+    sarea=math.pi*4*r*r
+    return sarea
+
+def spherecir(r):
+    import math
+    circum=math.pi*2*r
+    return circum
