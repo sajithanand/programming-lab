7ae0395c7329481012afffbb38a42b15088e02a4
sajithanand <94071830+sajithanand@users.noreply.github.com>
2022-02-02 14:36:58 +0530

    Add files via upload

---
+++ Graphics/Circle.py
@@ -0,0 +1,7 @@
+def cirarea(r):
+    carea=3.14*r*r
+    return carea
+
+def cirperi(r):
+    cperimeter=2*3.14*r
+    return cperimeter
---
+++ Graphics/rectangle.py
@@ -0,0 +1,8 @@
+def rectarea(l,b):
+    rarea=l*b
+    return rarea
+    
+
+def rectperi(l,b):
+    rperimeter=2*(l+b)
+    return rperimeter
